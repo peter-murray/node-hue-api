86c64a015492c97b7bccc5976657b165de980047
Peter Murray <pmurray@osirisoft.com>
2013-04-04 21:04:25 +0100

    - Updating default test values so that tests will work for me

--- test/support/testValues.js
+++ test/support/testValues.js
@@ -1,6 +1,6 @@
 module.exports = {
     host         : "192.168.2.129",
-    username     : "083b2f780c78555d532b78544f135798",
-    lightsCount  : 4,
+    username     : "08a902b95915cdd9b75547cb50892dc4",
+    lightsCount  : 5,
     locateTimeout: 5000
 };
